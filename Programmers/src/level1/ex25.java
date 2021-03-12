29302d3ad1f7fc1a59dc9fdb687e3ef5308f7033
GDana <danmi2857@naver.com>
2021-03-12 18:39:15 +0900

    [프로그래머스] 평균 구하기 - 완료

---
+++ Programmers/src/level1/ex25.java
@@ -0,0 +1,30 @@
+package level1;
+
+public class ex25 {
+
+	public static void main(String[] args) {
+		// 코딩테스트 연습 > 연습문제 > 평균 구하기 
+		
+		int[] arr1 = {1,2,3,4};
+		int[] arr2 = {5,5};
+
+		solution(arr1);
+		solution(arr2);
+		
+	}
+	
+	public static double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        
+        for(int i = 0; i< arr.length; i++) {
+        	sum += arr[i];
+        }
+        answer = (double) sum / arr.length;
+        
+        System.out.println(sum + "/ " + arr.length + " = " + answer);
+        
+        return answer;
+    }
+
+}
