4a7182812a3ccf85db4f484b856d3237adb67a87
GDana <danmi2857@naver.com>
2021-03-21 23:29:34 +0900

    [프로그래머스] 제일 작은 수 제거하기 - 다시풀기 완료

--- Programmers/src/level1/retry/ex19.java
+++ Programmers/src/level1/retry/ex19.java
@@ -7,7 +7,9 @@ public class ex19 {
 
 	public static void main(String[] args) {
 		// 코딩테스트 연습 > 연습문제 > 제일 작은 수 제거하기 
+		// 다시풀기 완료
 		/*
+		 * ::::: 처음 작성한 알고리즘 :::::
 		 * 1. arr.lenth == 0 || arr[0] == 10 이면
 		 * 	-1을 반환 
 		 * 2. array -> list 변환 
@@ -34,12 +36,53 @@ public class ex19 {
 		
 		int[] arr1 = {4,3,2,1};
 		int[] arr2 = {10};
+		int[] arr3 = {};
+		int[] arr4 = {3,1,3,4};
 		
-		solution(arr1);
-		solution(arr2);
+		solution(arr4);
+		//solution2(arr2);
+		//solution2(arr3);
 	}
 	
+	public static int[] solution2(int[] arr) {
+		System.out.println("::::: 다시 풀기(1) :::::");
+		int[] answer = {};
+		
+		if(arr.length <= 1 || (arr.length == 1 && arr[0] == 10)) {
+			answer = new int[] {-1};
+			System.out.println(Arrays.toString(answer) + "\n");
+			return answer;
+		}
+		
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int item : arr) {
+			list.add(item);
+		}
+
+		int index = 0;
+		for (int i = 1; i < list.size(); i++) {
+			if(list.get(index) > list.get(i)) {
+				index = i;
+			}
+		}
+		System.out.print("제일 작은 수: " + list.get(index) + "\n");
+
+		list.remove(index);
+
+		int size = 0;
+		answer = new int[list.size()];
+
+		for (int item : list) {
+			answer[size++] = item;
+		}
+		System.out.println(Arrays.toString(answer) + "\n");
+        
+        return answer;
+    }
+	
 	public static int[] solution(int[] arr) {
+		System.out.println("::::: 처음 제출했던 코드 :::::");
 		int[] answer = {};
         ArrayList<Integer> list = new ArrayList<>();
         
@@ -51,9 +94,12 @@ public class ex19 {
         for(int i = 0; i < list.size(); i++) {
         	for(int j = list.size()-1; j > i; j--) {
 	        	if(list.get(j) < list.get(j-1)) {
+	        		System.out.print(list.get(j) + " < " + list.get(j-1) + "\n");
 	        		index = j;
+	        		System.out.println("index: " + index);
 	        	}
         	}
+        	System.out.println("END::outer for");
         }
         
         System.out.print("제일 작은 수: " + list.get(index) + "\n");
