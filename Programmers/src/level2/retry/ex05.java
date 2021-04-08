1b4805c6eb44833ec152b7d415f9728a7969b6d0
GDana <danmi2857@naver.com>
2021-04-08 23:03:22 +0900

    [프로그래머스] 숫자의 표현 - 푸는 중

---
+++ Programmers/src/level2/retry/ex05.java
@@ -0,0 +1,15 @@
+package level2.retry;
+
+public class ex05 {
+
+	public static void main(String[] args) {
+		// 코딩테스 연습 > 연습문제 > 숫자의 표현 
+		
+		solution(15);
+	}
+
+	public static int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
