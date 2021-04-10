c9dc7d28f0f6092678a861f5d58201e28789d7d3
GDana <danmi2857@naver.com>
2021-04-10 21:54:11 +0900

    [프로그래머스] 땅따먹기 - 푸는중

---
+++ Programmers/src/level2/retry/ex06.java
@@ -0,0 +1,19 @@
+package level2.retry;
+
+public class ex06 {
+
+	public static void main(String[] args) {
+		// 코딩테스트 연습 > 연습문제 > 땅따먹기 
+
+		int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+	}
+
+	static int solution(int[][] land) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
