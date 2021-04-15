03bab8568f0d76da3c6a22fba2c835adf07cfc22
GDana <danmi2857@naver.com>
2021-04-15 21:41:44 +0900

    [프로그래머스] 다음 큰 숫자 - 푸는 중

---
+++ Programmers/src/level2/retry/ex07.java
@@ -0,0 +1,16 @@
+package level2.retry;
+
+public class ex07 {
+
+	public static void main(String[] args) {
+		// 코딩테스트 연습 > 연습문제 > 다음 큰 숫자 
+
+		solution(78);
+		solution(15);
+	}
+
+	public static int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
