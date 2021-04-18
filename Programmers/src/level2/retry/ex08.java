aa4d03698e10c85a7d9a61711ad587c182a4a782
GDana <danmi2857@naver.com>
2021-04-18 23:19:55 +0900

    [프로그래머스] 행렬의 곱셈 - 푸는 중

--- Programmers/src/level2/retry/ex08.java
+++ Programmers/src/level2/retry/ex08.java
@@ -5,7 +5,10 @@ public class ex08 {
 	public static void main(String[] args) {
 		// 코딩테스트 연습 > 연습문제 > 행렬의 곱셈
 
-		
+		int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+		int[][] arr2 = {{3, 3}, {3, 3}}; 
+		int[][] arr3 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
+		int[][] arr4 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
 	}
 
 	public static int[][] solution(int[][] arr1, int[][] arr2) {
