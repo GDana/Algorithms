3d0d6b0b206d88c9249ff1683dd9b7ad74911feb
GDana <danmi2857@naver.com>
2021-04-19 23:13:27 +0900

    [프로그래머스] 행렬의 곱셈 - 푸는 중

--- Programmers/src/level2/retry/ex08.java
+++ Programmers/src/level2/retry/ex08.java
@@ -9,6 +9,9 @@ public class ex08 {
 		int[][] arr2 = {{3, 3}, {3, 3}}; 
 		int[][] arr3 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
 		int[][] arr4 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+		
+		solution(arr1, arr2);
+		solution(arr3, arr4);
 	}
 
 	public static int[][] solution(int[][] arr1, int[][] arr2) {
