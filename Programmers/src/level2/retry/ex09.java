8524584b3acef8b1c8ef85c7223d7a24e01697eb
GDana <danmi2857@naver.com>
2021-04-20 23:07:02 +0900

    [프로그래머스] 주식가격 - 푸는 중

---
+++ Programmers/src/level2/retry/ex09.java
@@ -0,0 +1,17 @@
+package level2.retry;
+
+public class ex09 {
+
+	public static void main(String[] args) {
+		// 코딩테스트 연습 > 스택/큐 > 주식가격 
+		
+		int[] prices = {1, 2, 3, 2, 3};
+		
+		solution(prices);
+	}
+
+	public static int[] solution(int[] prices) {
+        int[] answer = {};
+        return answer;
+    }
+}
