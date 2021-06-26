c239df974171fb9fe53cabac5f8c9f463dab0862
GDana <danmi2857@naver.com>
2021-06-26 15:54:00 +0900

    [리뷰] 속도 개선

--- Baekjoon/src/done/A10845_1.java
+++ Baekjoon/src/done/A10845_1.java
@@ -8,12 +8,143 @@ import java.util.StringTokenizer;
 
 public class A10845_1 {
 
-	/**
-	 * 
-	 * 성공, 344ms 
-	 * System.out.println 보다 빠른 BufferedReader 를 사용하는 것이 올바른 접근 
-	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		test3();
+	}
+
+	/**
+	 * 성공, 196ms 
+	 * - test2() 처럼 각 이벤트마다 출력해줄 필요없음, 속도 느림 (412ms)
+	 * - 반복문 실행 완료 후 출력해줘도 됨(test3은 196ms)
+	 * - String으로 println 비추 -> StringBuilder 로 println 추천 
+	 */
+	public static void test3() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		System.out.println("----- 명령의 수를 입력하세요. -----");
+		int max = Integer.parseInt(br.readLine());
+		
+		LinkedList<Integer> que = new LinkedList<>();
+		
+		for(int i = 0; i < max; i++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str);
+			String command = st.nextToken();
+			
+			if(command.equals("push")) {
+				int num = Integer.parseInt(st.nextToken());
+				
+				if(que.size() < max) {
+					que.offer(num);
+				}
+			} else if(command.equals("pop")) {
+				if(que.isEmpty()) {
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(que.poll()).append("\n");
+				}
+			} else if(command.equals("size")) {
+				if(que.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(que.size()).append("\n");
+				}
+			} else if(command.equals("empty")) {
+				if(que.isEmpty()) {
+					sb.append(1).append("\n");
+				} else {
+					sb.append(0).append("\n"); 					
+				}
+			} else if(command.equals("front")) {
+				if(que.isEmpty()) {
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(que.peek()).append("\n");					
+				}
+			} else if(command.equals("back")) {
+				if(que.isEmpty()) {
+					sb.append(-1).append("\n");
+				} else {
+					sb.append(que.getLast()).append("\n"); 										
+				}
+			} 
+		}
+		System.out.println(sb);
+		br.close();
+	}
+	
+	/**
+	 * 성공, 412ms 
+	 */
+	public static void teest2() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int max = Integer.parseInt(br.readLine());
+		
+		LinkedList<Integer> que = new LinkedList<>();
+		
+		for(int i = 0; i < max; i++) {
+			StringBuilder sb = new StringBuilder();
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str);
+			String command = st.nextToken();
+			
+			if(command.equals("push")) {
+				int num = Integer.parseInt(st.nextToken());
+				
+				if(que.size() < max) {
+					que.offer(num);
+				}
+			} else if(command.equals("pop")) {
+				if(que.isEmpty()) {
+					sb.append(-1);
+					System.out.println(sb);
+				} else {
+					sb.append(que.poll());
+					System.out.println(sb);
+				}
+			} else if(command.equals("size")) {
+				if(que.isEmpty()) {
+					sb.append(0);
+					System.out.println(sb);
+				} else {
+					sb.append(que.size());
+					System.out.println(sb);
+				}
+			} else if(command.equals("empty")) {
+				if(que.isEmpty()) {
+					sb.append(1);
+					System.out.println(sb);
+				} else {
+					sb.append(0);
+					System.out.println(sb);
+				}
+			} else if(command.equals("front")) {
+				if(que.isEmpty()) {
+					sb.append(-1);
+					System.out.println(sb);
+				} else {
+					sb.append(que.peek());
+					System.out.println(sb);
+				}
+			} else if(command.equals("back")) {
+				if(que.isEmpty()) {
+					sb.append(-1);
+					System.out.println(sb);
+				} else {
+					sb.append(que.getLast()); 		
+					System.out.println(sb);
+				}
+			} 
+		}
+		br.close();
+	}
+	
+	/**
+	 * 성공, 344ms 
+	 */
+	public static void test1() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		System.out.println("----- 명령의 수를 입력하세요. -----");
@@ -65,5 +196,4 @@ public class A10845_1 {
 			} 
 		}
 	}
-
 }
